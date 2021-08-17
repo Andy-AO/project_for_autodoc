499765cfddc1db62d9d2d13f8acddc4b898e1cf3
zen96285 <zen96285@gmail.com>
2021-08-18 07:55:16 +0800

    my_module_for_test.py

---
+++ my_module_for_test.py
@@ -0,0 +1,22 @@
+"""
+Used to test the document generation.
+"""
+
+
+class Node:
+    """
+    Class document
+    """
+
+    def test_method(self):
+        """
+        Method documentation
+        """
+        pass
+
+
+    def test_function():
+        """
+        Function document
+        """
+        pass
